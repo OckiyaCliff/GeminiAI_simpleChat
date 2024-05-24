9e3146caf1b9ef738998fa92f74d895701c21f31
Cliff <Ockiyacliffc@gmail.com>
2024-05-24 18:57:19 +0100

    initial  commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <section className="search-section">
+        <p>what do you want to do know?
+          <button className="surprise">Surprise me!</button>
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
